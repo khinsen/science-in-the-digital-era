8d3beb2caf2acee9da319f0d5efa4ea21ecc1d98
Konrad Hinsen <konrad.hinsen@fastmail.net>
2022-04-15 09:35:26 +0200

    Use file modification timestamp for not-yet-commited pages
    For local builds (testing) only. Published versions must be built
    from fully commited pages.

--- pages2tiddlers.py
+++ pages2tiddlers.py
@@ -3,6 +3,8 @@ import os
 import re
 from datetime import datetime
 
+local_build = len(sys.argv) > 1 and sys.argv[1] == 'local'
+
 md_link = re.compile('\[(.+)\]\(([^ ]+)(?: "(.+)")?\)')
 page_directory = 'pages'
 tiddler_directory = 'tiddlers'
@@ -16,12 +18,15 @@ for tiddler_file in os.listdir(tiddler_directory):
     os.unlink(os.path.join(tiddler_directory, tiddler_file))
 
 for page_file in os.listdir(page_directory):
+    page_path = os.path.join(page_directory, page_file)
     page_title = os.path.splitext(page_file)[0]
     print(page_title)
-    page_text = open(os.path.join(page_directory, page_file)).read()
-    stream = os.popen('git log --format=%at -- "' + os.path.join(page_directory, page_file) + '"')
+    page_text = open(page_path).read()
+    stream = os.popen('git log --format=%at -- "' + page_path + '"')
     timestamps = stream.readlines()
     stream.close()
+    if len(timestamps) == 0 and local_build:
+        timestamps.append(os.path.getmtime(page_path))
     tw_timestamps = [datetime.fromtimestamp(int(timestamp)).strftime('%Y%m%d%H%M%S%f')[:-3] for timestamp in timestamps]
     tw_creation_timestamp = tw_timestamps[-1]
     tw_modification_timestamp = tw_timestamps[0]
@@ -29,7 +34,7 @@ for page_file in os.listdir(page_directory):
         tiddler_file.write(md_link.sub(rewrite_link, page_text))
     with open(os.path.join(tiddler_directory, page_title + '.meta'), 'w') as meta_file:
         meta_file.write(f'title: {page_title}\n')
+        meta_file.write('type: text/x-markdown\n')
         meta_file.write(f'created: {tw_creation_timestamp}\n')
         meta_file.write(f'modified: {tw_modification_timestamp}\n')
-        meta_file.write('type: text/x-markdown\n')
 
