ea18222d7a90206c654818a61529ee27d1ace193
Konrad Hinsen <konrad.hinsen@fastmail.net>
2023-11-21 11:33:35 +0100

    Change license to CC BY-NC-SA and summarize it

--- pages2chapters.py
+++ pages2chapters.py
@@ -62,9 +62,16 @@ The second reason is that empty pages are useful link targets, due to the backli
 
 ### License
 
-The pages of this digital garden are covered by a Creative Commons License ([CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/) to be precise).
+The pages of this digital garden are covered by the Creative Commons License [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/).
 
+According to this license, you are free to:
+- Share: copy and redistribute the material in any medium or format.
+- Adapt: remix, transform, and build upon the material.
 
+under the following conditions:
+- Attribution: You must give appropriate credit.
+- NonCommercial: You may not use the material for commercial purposes.
+- ShareAlike: You must distribute your contributions under the same license.
 """
 
 with open(output_path, 'a') as output_file:
